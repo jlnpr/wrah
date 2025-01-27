b12c8ea96f9b838918eee47043dff5b68422ebd8
jlnpr <jnapora@st.swps.edu.pl>
2025-01-27 10:22:41 +0100

    zadania lab_10

--- folder_projektu/folder_projektu/settings.py
+++ folder_projektu/folder_projektu/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
